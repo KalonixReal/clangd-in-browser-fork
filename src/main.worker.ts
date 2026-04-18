21e85e475d3599e267381533731cda1710b618f7
Kalonix <96559417+KalonixReal@users.noreply.github.com>
2026-04-18 21:45:18 +1200

    Update wasmBase and wasmUrl to new locations

--- src/main.worker.ts
+++ src/main.worker.ts
@@ -6,8 +6,8 @@ import { BrowserMessageReader, BrowserMessageWriter} from "vscode-languageserver
 
 declare var self: DedicatedWorkerGlobalScope;
 
-const wasmBase = `${import.meta.env.BASE_URL}wasm/`;
-const wasmUrl = `https://github.com/KalonixReal/clangd-in-browser-fork/releases/download/v1/clangd.wasm`;
+const wasmBase = `https://clangd.guyutongxue.site/wasm/`;
+const wasmUrl = `${wasmBase}clangd.wasm`;
 const jsModule = import(  /* @vite-ignore */ `${wasmBase}clangd.js`);
 
 // Pre-fetch wasm, and report progress to main
